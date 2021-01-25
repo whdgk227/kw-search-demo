a84b71a69cf38c341da624cc049986eed934bd3b
lch0120 <lch0120@midasit.com>
2021-01-25 14:39:22 +0900

    기본 페이지 추가, gitignore 추가

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
 
+from flask import make_response, render_template, request, send_file
+
+
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, Keyword!"
+
+
+@app.route('/search')
+def search():
+    headers = {'Content-Type': 'text/html'}
+    templates = render_template("search.html")
+    return make_response(templates, 200, headers)
