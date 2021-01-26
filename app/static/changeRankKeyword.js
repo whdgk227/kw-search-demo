b938dd3851fbc2291369c2cb556c728fcc6c28a7
whdgk227 <whdgk227@gmail.com>
2021-01-26 18:40:05 +0900

    add javascript for changing keyword rank text.

---
+++ app/static/changeRankKeyword.js
@@ -0,0 +1,7 @@
+  function changeRankKeyword(){
+    document.getElementById("1stRankText").innerHTML=document.getElementById("inputText").value;
+    document.getElementById("3rdRankText").innerHTML=document.getElementById("inputText").value;
+    document.getElementById("2ndRankText").innerHTML=document.getElementById("inputText").value;
+    document.getElementById("4thRankText").innerHTML=document.getElementById("inputText").value;
+    document.getElementById("5thRankText").innerHTML=document.getElementById("inputText").value;
+  }
