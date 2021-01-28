fb2bc649289c887ae73e0b0f40becf097be7c7e0
whdgk227 <whdgk227@gmail.com>
2021-01-28 15:06:21 +0900

    -

--- app/static/searchingKeyword.py
+++ app/static/searchingKeyword.py
@@ -12,19 +12,22 @@ df_RepKeyword = pd.read_excel("../../keyword_table/xlsx/Table_RepKeyword_xlsx.xl
 df_SearchingKeyword = pd.read_excel("../../keyword_table/xlsx/Table_SearchingKeyword_xlsx.xlsx", header=0)
 searching_keyword_list = df_SearchingKeyword["searching_keyword"]
 sizeSearchingKeyword = searching_keyword_list.shape[0]
-
 def get_scores_dict(searching_keyword_list, target_word):
     distance_dict = {}
     cnt=0
     for searching_keyword in searching_keyword_list:
-        distance_dict[cnt] = round(jamo_levenshtein(target_word, searching_keyword), 2)
-        if(searching_keyword.startswith(target_word)):
-            distance_dict[cnt] = round(distance_dict[cnt]*0.05,3)
-        elif(target_word in searching_keyword):
-            distance_dict[cnt] = round(distance_dict[cnt]*0.3,3)
-        if(searching_keyword==target_word):
-            distance_dict[cnt] = 0
-        cnt+=1
+        if target_word in searching_keyword:
+            if searching_keyword==target_word:
+                distance_dict[cnt] = 0
+            elif searching_keyword.startswith(target_word):
+                distance_dict[cnt] = round(jamo_levenshtein(target_word, searching_keyword)*0.05,3)
+            else:
+                distance_dict[cnt] = round(jamo_levenshtein(target_word, searching_keyword)*0.3,3)
+        elif abs(len(target_word) - len(searching_keyword)) < 5:
+            distance_dict[cnt] = round(jamo_levenshtein(target_word, searching_keyword),3)
+        else:
+            pass
+        cnt += 1
 
     return distance_dict
 
@@ -37,18 +40,24 @@ def preprocessing_target_word(target_word):
 def preprocessing_searching_keyword(searching_keyword_list):
     return searching_keyword_list.str.replace(" ","").str.lower()
 
-def searchKeyword(target_word, searching_keyword_list, out_length = 5, out_msg = False):
+def searchKeyword(target_word, searching_keyword_list, out_length = 5, out_msg = False, debug=False):
     if(out_length > 20):
         print('3rd parameter(out_length) must be under 20!')
         return
 
     start = time.time()
+    length_result = 0
     output_keyword_list = ['' for _ in range(out_length)]
     output_score_list = ['' for _ in range(out_length)]
+    output_keyword_sn_list = ['' for _ in range(out_length)]
+    output_searching_keyword_sn_list = ['' for _ in range(out_length)]
 
     #Preprocessing
     target_word = preprocessing_target_word(target_word)
     searching_keyword_list = preprocessing_searching_keyword(searching_keyword_list)
+    if debug:
+        time_pre = time.time()
+        print("pre:", time_pre-start)
 
     #Calculate Score
     distance_dict = get_scores_dict(searching_keyword_list, target_word)
@@ -56,18 +65,25 @@ def searchKeyword(target_word, searching_keyword_list, out_length = 5, out_msg =
     #Sort Score
     sort_output = sort_dict(distance_dict)
 
-    for i in range(out_length):
+    for i in range(len(sort_output)):
+        if (length_result >= out_length):
+            break
         rep_keyword_sn = df_SearchingKeyword["representation_keyword_sn"][sort_output[i][0]]
-        output_keyword_list[i] = df_RepKeyword[df_RepKeyword["representation_keyword_sn"]==rep_keyword_sn].iloc[0]["representation_keyword"]
-        output_score_list[i] = sort_output[i][1]
+        if (rep_keyword_sn in output_keyword_sn_list):
+            continue
+        output_searching_keyword_sn_list[length_result] = df_SearchingKeyword["searching_keyword"][sort_output[i][0]]
+        output_keyword_list[length_result] = df_RepKeyword[df_RepKeyword["representation_keyword_sn"]==rep_keyword_sn].iloc[0]["representation_keyword"]
+        output_keyword_sn_list[length_result] = rep_keyword_sn
+        output_score_list[length_result] = sort_output[i][1]
+        length_result+=1
 
     # print results
     if (out_msg):
         print("Target Word : ", target_word)
         for i in range(out_length):
-            print("searching_keyword_sn : ", sort_output[i][0], ", score : ", sort_output[i][1], ", Rep_keyword : ", output_keyword_list[i])
+            print("Rep_keyword : ", output_keyword_list[i], ", searching keyword : ", output_searching_keyword_sn_list[i], ", score : ", output_score_list[i], ", rep_keyword_sn : ", output_keyword_sn_list[i])
 
         print(f"소요시간: {time.time() - start:.4f}")
     return output_keyword_list, output_score_list
 
-searchKeyword(target_keyword, searching_keyword_list, out_length, out_msg=True)
+searchKeyword(target_keyword, searching_keyword_list, out_length, out_msg=True, debug=False)
--- app/static/searchingKeyword.sh
+++ app/static/searchingKeyword.sh
@@ -1,9 +1,9 @@
 # Searching Keyword
-# Example use : sh searchingKeyword.sh "TargetKeyword"
+# Example use : sh searchingKeyword.sh out_length "TargetKeyword"
 
 
+OUT_LENGTH=5 # keyword
 TARGET_KEYOWRD=$1 # keyword
-OUT_LENGTH=$2 # keyword
 # OUTPUT_PATH=$2
 
 DIR="$( unset CDPATH && cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
